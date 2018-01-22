5d2e3ec796574fb524ab3b24db0a46ab8fbd9b59
ShreeramRaghunathan <shreeramr05@gmail.com>
2018-01-22 18:08:37 +0530

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginformComponent } from './login/loginform/loginform.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginformComponent,
+    MainpageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot([
+		{
+			path: '',
+			redirectTo: 'mainpage', 
+			pathMatch: 'full'
+		},
+		{
+			path: 'mainpage',
+			component: MainpageComponent
+		},
+		{
+			path: 'login/loginform',
+			component: LoginformComponent
+		}
+    ])
   ],
+  exports: [ RouterModule ],
   providers: [],
   bootstrap: [AppComponent]
 })
